1667a830d0a46bfce6fd9e466325d276d73433d3
dexter-idc <140709325+Parthipan-olt@users.noreply.github.com>
2023-10-12 12:16:54 +0530

    Added Js

---
+++ src/script.js
@@ -0,0 +1,48 @@
+function fetchData(urlFinal) {
+    $.ajax({
+        url: urlFinal,
+        type: 'GET',
+        dataType: 'json',
+        success(data) {
+            const filteredResults = filterResultsByTitle(data.results);
+            displayResults(filteredResults); // Display the filtered results
+        }
+    });
+}
+
+function getSearchQuery() {
+    const apiKey = 'b31738b39a9616ae7b1e0f4528fb1985';
+    const query = $('#searchBox').val();
+    const language = selectedLanguage(); // Get the selected language
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&language=${language}`;
+    fetchData(url);
+}
+
+$("#searchButton").click(function (e) {
+    e.preventDefault();
+    getSearchQuery();
+});
+
+function selectedLanguage() {
+    return $('input[name="language"]:checked').val();
+}
+
+function filterResultsByTitle(results) {
+    const query = $('#searchBox').val().toLowerCase();
+    return results.filter(movie => movie.title.toLowerCase().includes(query));
+}
+
+function displayResults(data) {
+    const movieResultsContainer = $('.search-results');
+    movieResultsContainer.empty();
+
+    if (data.length === 0) {
+        movieResultsContainer.text("No results found.");
+    } else {
+        data.forEach(movie => {
+            const title = movie.title;
+            const titleElement = $('<p>').text(title);
+            movieResultsContainer.append(titleElement);
+        });
+    }
+}
