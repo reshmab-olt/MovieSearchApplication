0a0a9e69fdf7a2f24b076b22585a94e778b0cda0
Reshma B <139335112+reshmab-olt@users.noreply.github.com>
2023-10-16 10:25:39 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,131 +1,157 @@
 const apiKey = 'b31738b39a9616ae7b1e0f4528fb1985';
-let genreClicked = false
-let genreName = 0;
+let currentPage = 1;
+let allResults = []; // To store results from all pages
+let genreClicked = false;
+let genre = null;
+let selectedPage = 1;
 
-function fetchData(urlFinal) {
-    $.ajax({
-        url: urlFinal,
-        type: 'GET',
-        dataType: 'json',
-        success(data) {
+// Function to update the UI and fetch movies based on the selected genre
+function filterByGenre(selectedGenre, currentPage) {
+    if (selectedGenre) {
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${selectedGenre}`;
+        fetchData(url, currentPage, selectedPage);
+    }
+}
 
+function pagination(results, currentPage, url, length, selectedPage) {
+    const movieResultsContainer = $('.search-results');
 
-            pagination(data);
+    movieResultsContainer.empty();
 
+    const totalPages = Math.ceil(length / 10);
+    console.log(totalPages)
+
+    const start = (selectedPage - 1) * 10;
+    const end = Math.min(start + 10, results.length);
+
+    const slicedResult = results.slice(start, end);
+    displayResults(slicedResult);
+    console.log(slicedResult, 'slice')
+    const buttonsToShow = 5;
+
+    if (totalPages > 1) {
+        for (let i = 1; i < totalPages; i++) {
+            const pageSelect = $('<button>').addClass('pageButtons btn btn-secondary px-2 py-1').attr('value', i).html(i);
+
+            if (i === currentPage) {
+                pageSelect.addClass('activePage');
+            }
+
+            if (i <= buttonsToShow || i === totalPages) {
+                movieResultsContainer.append(pageSelect);
+            } else if (i == buttonsToShow + 1) {
+                const ellipsis = $('<span>').html('...');
+                movieResultsContainer.append(ellipsis);
+            }
+        }
+        buttonCall(url);
+    } else {
+        displayResults(results);
+    }
+}
+
+function buttonCall(url) {
+    $('.pageButtons').on('click', function (e) {
+        const selectedPage = Math.ceil($(this).attr('value'));
+
+        if (currentPage !== selectedPage) {
+            if (selectedPage > 2) {
+                currentPage = (Math.ceil((selectedPage / 2)));
+                console.log(currentPage, 'cyrre')
+                fetchData(url, currentPage, selectedPage)
+            }
+        }
+        if (selectedPage == 1 || selectedPage == 2) {
+            fetchData(url, 1, selectedPage)
         }
     });
 }
 
-function getSearchQuery(apiKey) {
-    const query = $('#searchBox').val();
+function fetchData(url, page, selectedPage) {
+    finalUrl = url + `&page=${page}`;
+    console.log(finalUrl)
+    $.ajax({
+        url: finalUrl,
+        dataType: 'json',
+        success: function (data) {
+            const results = data.results;
+            allResults = allResults.concat(results);
+            pagination(allResults, page, url, data.total_pages, selectedPage);
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
-    fetchData(url);
+        },
+        error: function (error) {
+            console.error('Error fetching data:', error);
+        }
+    });
+}
+
+
+function displayResults(slicedResult) {
+    const movieResultsContainer = $('.search-results');
+
+    if (slicedResult.length === 0) {
+        movieResultsContainer.text("No results found.");
+        return;
+    } else {
+        movieResultsContainer.empty(); // Clear existing results
+        slicedResult.forEach(movie => {
+            const imageBaseUrl = 'http://image.tmdb.org/t/p/';
+            const posterPath = movie.poster_path;
+            const imageSource = posterPath ? `${imageBaseUrl}w154${posterPath}` : 'placeholder-image-url.jpg';
+            const title = movie.title;
+            const movieId = movie.id;
+            if (posterPath) {
+                const movieContainer = $('<div>').addClass('movie-container');
+                movieContainer.empty();
+                const imageElement = $('<img>').attr('src', imageSource);
+                const titleElement = $(`<p class="movie-title" data-movie-id="${movieId}">`).text(title);
+                movieContainer.append(imageElement);
+                movieContainer.append(titleElement);
+                movieResultsContainer.append(movieContainer);
+            }
+        });
+    }
 }
 
 $("#searchButton").click(function (e) {
     e.preventDefault();
-    getSearchQuery(apiKey);
+    let currentPage = 1; // Reset the current page when searching
+    allResults = []; // Reset the results
+    const searchQuery = $('#searchInput').val();
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`;
+    fetchData(url, currentPage);
 });
 
-function filterResultsByTitle(results) {
-    const query = $('#searchBox').val().toLowerCase();
-    return results.filter(movie => movie.title.toLowerCase().includes(query));
-}
+$(document).on('click', '.movie-title', function () {
+    const movieId = $(this).data('movie-id');
+    // Open the movie-details.html page with the movie ID as a query parameter
+    window.location.href = `movie-details.html?id=${movieId}`;
+});
 
-function displayResults(data) {
-    const movieResultsContainer = $('.search-results');
-    movieResultsContainer.empty();
-
-    if (data.length === 0) {
-        movieResultsContainer.text("No results found.");
-    } else {
-        data.forEach(movie => {
-            const imageBaseUrl = 'http://image.tmdb.org/t/p/';
-            const posterPath = movie.poster_path;
-            const imageSource = posterPath ? `${imageBaseUrl}w154${posterPath}` : 'placeholder-image-url.jpg';
-            const movieContainer = $('<div>').addClass('movie-container');
-            const title = movie.title;
-            if (posterPath) {
-                const imageElement = $('<img>').attr('src', imageSource);
-                const titleElement = $('<p>').text(title);
-                movieContainer.append(imageElement);
-                movieContainer.append(titleElement);
-            }
-
-            movieResultsContainer.append(movieContainer);
-        });
+// Click event for the "More" button
+const moreElement = document.querySelector('#more');
+moreElement.addEventListener('click', function () {
+    let currentPage = 1; // Reset current page
+    if (genre) {
+        fetchData(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genre}`, currentPage);
     }
-    
-        data.forEach(movie => {
-            const imageBaseUrl = 'http://image.tmdb.org/t/p/';
-            const posterPath = movie.poster_path;
-            const imageSource = posterPath ? `${imageBaseUrl}w154${posterPath}` : 'placeholder-image-url.jpg';
-            const movieContainer = $('<div>').addClass('movie-container');
-            const title = movie.title;
-    
-            if (posterPath) {
-                const imageElement = $('<img>').attr('src', imageSource);
-                const titleElement = $('<p>').text(title);
-    
-                const detailLink = $('<a>').attr('href', `detail.html?title=${title}`).append(imageElement, titleElement);
-    
-                movieContainer.append(detailLink);
-            }
-    
-            movieResultsContainer.append(movieContainer);
-        });
-    }
-    
-// Sort function
+});
+
+
 $('.sort').on('click', function () {
-    genreClicked = true
-    genre = $(this).attr('value');
-    console.log(genre)
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genre}`;
-    fetchData(url);
-});
+    currentPage = 1; // Reset current page
+    genreClicked = true;
+    genre = Math.round($(this).attr('value'));
+    filterByGenre(genre, currentPage); // Fetch data with the selected genre
 
-function pagination(data) {
-
-    const slicedResult = data.results.slice(0, 5);
-    if (genreClicked) {
-        displayResults(slicedResult)
-
-        genreClicked = false
-    }
-    const filteredTitleResult = filterResultsByTitle(slicedResult);
-    displayResults(filteredTitleResult)
-    const movieResultsContainer = $('.search-results')
-    totalPages = data.results.length / 10;
-
-    const pageNumberDiv = $('<div>').addClass('page-number-div').css('display', 'inline');
-    for (let i = 1; i <= totalPages; i++) {
-        const pageSelect = $('<button>').addClass('pageButtons btn btn-secondary px-2 py-1').attr('value', i).html(i);
-
-        pageNumberDiv.append(pageSelect)
-    }
-    $(document.body).append(pageNumberDiv)
-}
-
-const moreElement = document.querySelector('#more'); 
-moreElement.addEventListener('click', function() {
-    const moreFilters = document.querySelector('.more-filters'); 
-    moreFilters.style.display = 'block';
 });
 
 $('.filter-options').on('click', function () {
-    genreClicked = true
-    genre = $(this).attr('value');
-    console.log(genre)
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genre}`;
-    fetchData(url);
-    const moreFilters = document.querySelector('.more-filters'); 
-    moreFilters.style.display = 'none';
+    const searchQuery = $('#searchInput').val();
+    if (searchQuery.trim() !== "") {
+        let currentPage = 1; // Reset current page
+        genreClicked = true;
+        genre = $(this).attr('value');
+        filterByGenre(genre, currentPage);
+    }
 });
-
-$('#carousel').on('click', function () {
-    const moreFilters = document.querySelector('.more-filters');
-    moreFilters.style.display = 'none';
-});
-
