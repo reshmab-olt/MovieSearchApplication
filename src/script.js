f2790e4bdbb5b21afc3be4706c4498954726ce00
Reshma B <139335112+reshmab-olt@users.noreply.github.com>
2023-10-16 17:10:06 +0530

    update script.js, index.html, styles.css

--- src/script.js
+++ src/script.js
@@ -1,99 +1,147 @@
+$("#searchButton").click(function (e) {
+    e.preventDefault();
+    const searchQuery = $('#searchInput').val();
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`;
+    fetchResults(url, 0, 1);
+});
+
 const apiKey = 'b31738b39a9616ae7b1e0f4528fb1985';
-let currentPage = 1;
-let allResults = []; // To store results from all pages
-let genreClicked = false;
-let genre = null;
-let selectedPage = 1;
 
-// Function to update the UI and fetch movies based on the selected genre
-function filterByGenre(selectedGenre, currentPage) {
-    if (selectedGenre) {
-        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${selectedGenre}`;
-        fetchData(url, currentPage, selectedPage);
-    }
-}
+let currentActivePage = 1;
+let totalPages = 500;
 
-function pagination(results, currentPage, url, length, selectedPage) {
-    const movieResultsContainer = $('.search-results');
+function getGenre() {}
 
-    movieResultsContainer.empty();
-
-    const totalPages = Math.ceil(length / 10);
-    console.log(totalPages)
-
-    const start = (selectedPage - 1) * 10;
-    const end = Math.min(start + 10, results.length);
-
-    const slicedResult = results.slice(start, end);
-    displayResults(slicedResult);
-    console.log(slicedResult, 'slice')
-    const buttonsToShow = 5;
-
-    if (totalPages > 1) {
-        for (let i = 1; i < totalPages; i++) {
-            const pageSelect = $('<button>').addClass('pageButtons btn btn-secondary px-2 py-1').attr('value', i).html(i);
-
-            if (i === currentPage) {
-                pageSelect.addClass('activePage');
-            }
-
-            if (i <= buttonsToShow || i === totalPages) {
-                movieResultsContainer.append(pageSelect);
-            } else if (i == buttonsToShow + 1) {
-                const ellipsis = $('<span>').html('...');
-                movieResultsContainer.append(ellipsis);
-            }
-        }
-        buttonCall(url);
-    } else {
-        displayResults(results);
-    }
-}
-
-function buttonCall(url) {
-    $('.pageButtons').on('click', function (e) {
-        const selectedPage = Math.ceil($(this).attr('value'));
-
-        if (currentPage !== selectedPage) {
-            if (selectedPage > 2) {
-                currentPage = (Math.ceil((selectedPage / 2)));
-                console.log(currentPage, 'cyrre')
-                fetchData(url, currentPage, selectedPage)
-            }
-        }
-        if (selectedPage == 1 || selectedPage == 2) {
-            fetchData(url, 1, selectedPage)
-        }
-    });
-}
-
-function fetchData(url, page, selectedPage) {
+function fetchResults(url, sliceStart, page) {
+    console.log(url);
     finalUrl = url + `&page=${page}`;
-    console.log(finalUrl)
+    console.log(finalUrl);
     $.ajax({
         url: finalUrl,
-        dataType: 'json',
+        datatype: 'json',
         success: function (data) {
-            const results = data.results;
-            allResults = allResults.concat(results);
-            pagination(allResults, page, url, data.total_pages, selectedPage);
-
+            results = data.results;
+            totalResultsLength = data.total_results;
+            createButtons(url, totalPages, totalResultsLength);
+            displayResults(results, sliceStart);
         },
         error: function (error) {
             console.error('Error fetching data:', error);
+            alert('No results');
         }
     });
 }
 
-
-function displayResults(slicedResult) {
+function createButtons(url, totalPages, totalResultsLength) {
     const movieResultsContainer = $('.search-results');
+    movieResultsContainer.empty();
+    const paginationContainer = $('.pagination-container');
+    paginationContainer.empty();
 
+    const maxVisibleButtons = 5;
+    const currentPage = currentActivePage;
+
+    if (totalPages > maxVisibleButtons) {
+        let startPage = Math.max(currentPage - Math.floor(maxVisibleButtons / 2), 1);
+        let endPage = Math.min(startPage + maxVisibleButtons - 1, totalPages);
+
+        if (endPage - startPage < maxVisibleButtons) {
+            startPage = endPage - maxVisibleButtons + 1;
+        }
+
+        if (startPage > 1) {
+            const firstPageButton = $('<button>').addClass('pageButtons').attr('value', 0).html('1');
+            paginationContainer.append(firstPageButton);
+        }
+
+        if (startPage > 2) {
+            const ellipsisStart = $('<span>').html('...');
+            paginationContainer.append(ellipsisStart);
+        }
+
+        for (let i = startPage; i <= endPage; i++) {
+            sliceStart = 0;
+            if (i % 2 == 0) {
+                sliceStart += 10;
+            }
+            const pageSelect = $('<button>').addClass('pageButtons').attr('value', sliceStart).html(i);
+
+            if (i === currentPage) {
+                pageSelect.addClass('active-page');
+            }
+            paginationContainer.append(pageSelect);
+        }
+
+        if (endPage < totalPages - 1) {
+            const ellipsisEnd = $('<span>').html('...');
+            paginationContainer.append(ellipsisEnd);
+        }
+
+        if (endPage < totalPages) {
+            const lastPage = totalPages;
+            sliceStart = 0;
+            if (totalPages % 2 == 0) {
+                sliceStart += 10;
+            }
+            const lastPageButton = $('<button>').addClass('pageButtons').attr('value', sliceStart).html(lastPage);
+            paginationContainer.append(lastPageButton);
+        }
+    } else {
+        for (let i = 1; i <= totalPages; i++) {
+            sliceStart = 0;
+            if (i % 2 == 0) {
+                sliceStart += 10;
+            }
+            const pageSelect = $('<button>').addClass('pageButtons').attr('value', sliceStart).html(i);
+            if (i === currentPage) {
+                pageSelect.addClass('activePage');
+            }
+            paginationContainer.append(pageSelect);
+        }
+    }
+
+    onPageNumberClick(url);
+}
+
+function onPageNumberClick(url) {
+    $('.pageButtons').on('click', function (e) {
+
+        let sliceStart = $(this).val();
+        let page = 1;
+        console.log(parseInt($(this).val()), 'slice')
+        currentpage = parseInt($(this).html());
+        currentActivePage = currentpage;
+        console.log(currentActivePage)
+        if (currentpage != 1 && currentpage != 2) {
+            if (currentpage % 2 != 0) {
+                page = currentpage - 1;
+                fetchResults(url, sliceStart, page);
+            }
+            if (currentpage % 2 == 0) {
+                page = currentpage - 2
+
+                fetchResults(url, sliceStart, page);
+            }
+        }
+        if (currentpage <= 2) {
+            page = 1
+            sliceStart = (currentpage - 1) * 10
+            fetchResults(url, sliceStart, page);
+        }
+    });
+}
+
+function displayResults(results, sliceStart) {
+
+    const movieResultsContainer = $('.search-results');
+    slicedResult = results.slice(sliceStart, sliceStart + 10)
     if (slicedResult.length === 0) {
-        movieResultsContainer.text("No results found.");
+
+        alert('No Results!!!')
+        movieResultsContainer.empty(); const paginationContainer = $('.pagination-container');
+        paginationContainer.empty();
         return;
     } else {
-        movieResultsContainer.empty(); // Clear existing results
         slicedResult.forEach(movie => {
             const imageBaseUrl = 'http://image.tmdb.org/t/p/';
             const posterPath = movie.poster_path;
@@ -103,7 +151,7 @@ function displayResults(slicedResult) {
             if (posterPath) {
                 const movieContainer = $('<div>').addClass('movie-container');
                 movieContainer.empty();
-                const imageElement = $('<img>').attr('src', imageSource);
+                const imageElement = $(`<img data-movie-id="${movieId}">`).addClass('movie-image').attr('src', imageSource);
                 const titleElement = $(`<p class="movie-title" data-movie-id="${movieId}">`).text(title);
                 movieContainer.append(imageElement);
                 movieContainer.append(titleElement);
@@ -111,47 +159,47 @@ function displayResults(slicedResult) {
             }
         });
     }
+
 }
-
-$("#searchButton").click(function (e) {
-    e.preventDefault();
-    let currentPage = 1; // Reset the current page when searching
-    allResults = []; // Reset the results
-    const searchQuery = $('#searchInput').val();
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`;
-    fetchData(url, currentPage);
-});
-
-$(document).on('click', '.movie-title', function () {
-    const movieId = $(this).data('movie-id');
-    // Open the movie-details.html page with the movie ID as a query parameter
-    window.location.href = `movie-details.html?id=${movieId}`;
-});
-
-// Click event for the "More" button
 const moreElement = document.querySelector('#more');
 moreElement.addEventListener('click', function () {
-    let currentPage = 1; // Reset current page
-    if (genre) {
-        fetchData(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genre}`, currentPage);
-    }
+    const moreFilters = document.querySelector('.more-filters');
+    moreFilters.style.display = 'block';
+    $(document.body).on('focusin', function () {
+        console.log('Displaying more filters');
+        moreFilters.style.display = 'none';
+    });
 });
 
-
 $('.sort').on('click', function () {
-    currentPage = 1; // Reset current page
-    genreClicked = true;
+    currentPage = 1;
     genre = Math.round($(this).attr('value'));
-    filterByGenre(genre, currentPage); // Fetch data with the selected genre
+    filterByGenre(genre, currentPage);
 
 });
 
 $('.filter-options').on('click', function () {
-    const searchQuery = $('#searchInput').val();
-    if (searchQuery.trim() !== "") {
-        let currentPage = 1; // Reset current page
-        genreClicked = true;
-        genre = $(this).attr('value');
-        filterByGenre(genre, currentPage);
+
+    let currentPage = 1;
+    genre = $(this).attr('value');
+    filterByGenre(genre, currentPage);
+});
+
+function filterByGenre(selectedGenre, currentPage) {
+    if (selectedGenre) {
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${selectedGenre}`;
+        fetchResults(url, 0, currentPage);
     }
+}
+
+$(document).on('click', '.movie-title, .movie-image', function () {
+    const movieId = $(this).data('movie-id');
+    console.log('movieId', movieId);
+
+    window.location.href = `detail.html?id=${movieId}`;
+});
+
+$('#carousel').on('click', function () {
+    const moreFilters = document.querySelector('.more-filters');
+    moreFilters.style.display = 'none';
 });
